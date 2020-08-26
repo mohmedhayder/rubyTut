2e2fd0be3a5a6528bc5abd326ed1ded159d57286
mohmedhayder <mohmedaiyaeldeen@gmail.com>
2020-08-26 11:19:58 +0300

    var hands on

---
+++ veriables.rb
@@ -0,0 +1,7 @@
+character_name = "Boy"
+character_age = "26"
+
+puts("There once was a man named "+ character_name)
+puts("he was "+ character_age + " years old.")
+puts("He really liked the name " + character_name)
+puts("but didn't like being " + character_age)
