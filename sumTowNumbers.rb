11b267f4dbe1a33734a8d5b56a9bcdcc65af17bb
mohmedhayder <mohmedaiyaeldeen@gmail.com>
2020-08-27 11:16:43 +0300

    script to sum two numbers entered by the user using ruby

---
+++ sumTowNumbers.rb
@@ -0,0 +1,7 @@
+puts "Enter the first number"
+firstNumber = gets.chomp().to_f
+puts "Enter the second number"
+secondNumber = gets.chomp().to_f
+result = firstNumber + secondNumber
+puts("the sum of the numbers is : " )
+puts result
